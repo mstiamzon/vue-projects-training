26b06d49f8843e099a570b08beba3ad1b592f6fd
mstiamzon <mary.tiamzon@neworientalclub88.com>
2020-05-14 18:09:52 +0800

    incomponent-routeguard progress

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -25,17 +25,25 @@
   </div>
 </template>
 <script>
-import { mapState, mapActions } from 'vuex'
-
+import { mapState } from 'vuex'
+import NProgress from 'nprogress'
+import store from '@/store/store'
 export default {
   props: ['id'],
-  created() {
-    this.fetchEvent(this.id)
+  beforeRouteEnter (to, from, next) {
+    NProgress.start()
+    store.dispatch('event/fetchEvent', to.params.id).then(() => {
+      NProgress.done()
+      next()
+    })
   },
+  // created() {
+  //   this.fetchEvent(this.id)
+  // },
   computed: mapState({
     event: state => state.event.event
-  }),
-  methods: mapActions('event', ['fetchEvent'])
+  })
+  // methods: mapActions('event', ['fetchEvent'])
 }
 </script>
 <style scoped>
